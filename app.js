c0290e6e3cd789e1225e2988a6dadc754a63d57d
masok87 <mdanca1987@gmail.com>
2023-01-26 10:00:42 +0000

    changed some things

--- app.js
+++ app.js
@@ -17,45 +17,66 @@ if (user == "Matt") {
 alert("Hello there " + user + "! I'm so glad you decided to visit my website!");
 
 // question one
-let filmQuestion = prompt("What is one my favourite films?.").toLowerCase();
+function filmQuestion() {
+  let Question1 = prompt("What is one my favourite films?.").toLowerCase();
 
-let filmsILike = ["whiplash", "come and see", "nightcrawler", "dunkirk"];
+  let filmsILike = ["whiplash", "come and see", "nightcrawler", "dunkirk"];
 
-for (let i = 0; i < filmsILike.length; i++) {
-  if (filmQuestion == filmsILike[i]) {
-    alert("Yes I love " + filmQuestion + "!");
-    userPoints++;
+  for (let i = 0; i < filmsILike.length; i++) {
+    if (Question1 == filmsILike[i]) {
+      alert("Yes I love " + Question1 + "!");
+      userPoints++;
+    }
   }
 }
 
 // question two
-let footballQuestion = confirm("Do I support Norwich");
+function footballQuestion() {
+  let Question2 = confirm("Do I support Norwich");
 
-if (footballQuestion == true) {
-  alert("Good.");
-  userPoints++;
-} else {
-  alert(">:(");
-}
-
-let houseQuestion = confirm("Have I lived abroad?");
-
-if (houseQuestion == true) {
-  alert("Indeed.");
-  userPoints++;
-} else {
-  alert("Nope.");
-}
-
-let foodQuestion = prompt("What is one my favourite foods?.").toLowerCase();
-
-let foodILike = ["ramen", "carbonara", "steak au poivre"];
-
-for (let i = 0; i < foodILike.length; i++) {
-  if (foodQuestion == foodILike[i]) {
-    alert(foodQuestion + " is peak.");
+  if (Question2 == true) {
+    alert("Good.");
     userPoints++;
+  } else {
+    alert(">:(");
+  }
+}
+function houseQuestion() {
+  let Question3 = confirm("Have I lived abroad?");
+
+  if (Question3 == true) {
+    alert("Indeed.");
+    userPoints++;
+  } else {
+    alert("Nope.");
+  }
+}
+function foodQuestion() {
+  let Question4 = prompt("What is one my favourite foods?.").toLowerCase();
+
+  let foodILike = ["ramen", "carbonara", "steak au poivre"];
+
+  for (let i = 0; i < foodILike.length; i++) {
+    if (Question4 == foodILike[i]) {
+      alert(Question4 + " is peak.");
+      userPoints++;
+    }
+  }
+}
+function danceQuestion() {
+  let Question5 = confirm("What do you think ,  do i like dancing? ");
+
+  if (Question5 == true) {
+    alert("Well done ! ");
+    userPoints++;
+  } else {
+    alert("ohh noo");
   }
 }
 
-alert("Well done " + user + "! You scored " + userPoints + "/4");
+filmQuestion();
+footballQuestion();
+houseQuestion();
+foodQuestion();
+danceQuestion();
+alert("Well done " + user + "! You scored " + userPoints + "/5");
